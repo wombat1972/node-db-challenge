1756c5db6705f7916d14fef86c4ffc44dec02864
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2020-03-08 20:09:14 -0400

    got it

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const server = express();
+const projectRouter = require("./projects/projects-router");
+
+server.use(express.json());
+server.use("/api/projects", projectRouter);
+
+module.exports = server;
